1624aa462cfefcee0db2d2776d4cdfd83034d6bc
Libra <libra_085925@163.com>
2019-07-04 11:32:23 +0800

    register page and custom font

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,103 +1,25 @@
 <template>
-  <el-col :gutter="20" class="login-page">
-    <el-col :span="16">
-      <div class="grid-content bg-purple">www</div>
-    </el-col>
-    <el-col :span="8" class="login-page-right">
-      <div class="login-input">
-        <el-card class="box-card">
-          <el-form
-            :model="ruleForm"
-            :rules="rules"
-            ref="ruleForm"
-            label-width="100px"
-            class="demo-ruleForm"
-          >
-            <el-form-item label="用户名" prop="name">
-              <el-input v-model="ruleForm.name"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="name">
-              <el-input v-model="ruleForm.name"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
+  <div class="login-page">
+    <div class="container">
+      <div class="login-form">
+        <div class="login-title">登 陆</div>
+        <div class="login-input-parts">
+          <input class="login-input" placeholder="请输入邮箱..." />
+          <input class="login-input" placeholder="请输入密码..." />
+          <input class="login-input button" type="button" value="登 陆" />
+        </div>
+        <div class="login-forgot">忘记密码？</div>
       </div>
-    </el-col>
-  </el-col>
+      <div class="login-signup">
+        没有账号?
+        <a href="#">注 册</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      ruleForm: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
-      },
-      rules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ],
-        region: [
-          { required: true, message: "请选择活动区域", trigger: "change" }
-        ],
-        date1: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择日期",
-            trigger: "change"
-          }
-        ],
-        date2: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择时间",
-            trigger: "change"
-          }
-        ],
-        type: [
-          {
-            type: "array",
-            required: true,
-            message: "请至少选择一个活动性质",
-            trigger: "change"
-          }
-        ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
-      }
-    };
-  },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    }
-  }
-};
+export default {};
 </script>
 
 <style lang="less" scoped>
---
+++ pages/register/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="register-page">
+    <div class="container">
+      <div class="register-form">
+        <div class="register-title">注册</div>
+        <div class="register-input-parts">
+          <input class="register-input" placeholder="请输入邮箱..." />
+          <input class="register-input" placeholder="请输入密码..." />
+          <input class="register-input" placeholder="请确认密码..." />
+          <input class="register-input button" type="button" value="注册" />
+        </div>
+      </div>
+      <div class="register-signup">
+        以有账号?
+        <a href="#">登陆</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import "./index.less";
+</style>
