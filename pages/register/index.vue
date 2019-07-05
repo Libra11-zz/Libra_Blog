91d6d7adb0846245a43301901a407dfebeab2347
Libra <libra_085925@163.com>
2019-07-05 19:06:37 +0800

    modify the register page

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -2,24 +2,47 @@
   <div class="register-page">
     <div class="container">
       <div class="register-form">
-        <div class="register-title">注册</div>
+        <div class="register-title">注 册</div>
         <div class="register-input-parts">
-          <input class="register-input" placeholder="请输入邮箱..." />
-          <input class="register-input" placeholder="请输入密码..." />
-          <input class="register-input" placeholder="请确认密码..." />
-          <input class="register-input button" type="button" value="注册" />
+          <el-input
+            class="register-input"
+            placeholder="请输入邮箱..."
+            prefix-icon="el-icon-date"
+            v-model="input1"
+          ></el-input>
+          <el-input
+            class="register-input"
+            placeholder="请输入密码..."
+            prefix-icon="el-icon-search"
+            v-model="input2"
+          ></el-input>
+          <el-input
+            class="register-input"
+            placeholder="请确认密码..."
+            prefix-icon="el-icon-search"
+            v-model="input3"
+          ></el-input>
+          <el-button>注 册</el-button>
         </div>
       </div>
       <div class="register-signup">
-        以有账号?
-        <a href="#">登陆</a>
+        已有账号?
+        <a href="#">登 陆</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      input1: "",
+      input2: "",
+      input3: ""
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
